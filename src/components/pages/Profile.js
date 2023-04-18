2f7a144a33687f10f63aab91a33ed75cd669c290
AhmedGaid1 <ahmedgaid2045@gmail.com>
2023-04-18 05:36:07 +0200

    sprint2

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,28 +1,73 @@
-import "./Testimonials.css";
-import TestimonialCard from "./TestimonialCard";
-
-const profile1 = [
-  {
-    image: "",
-    fname: "Ahmed",
-    lname: "Gaid",
-    email: "ahmedGaid2023@gmail.com",
-    phone: "002011111111111",
-    dateOfBirth: "1/2/2003",
-  },
-];
+import React, { useState } from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 
 const Profile = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [birthdate, setBirthdate] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
   return (
-    <section className="testimonials">
-      <div className="container grid">
-        <h2> Hello! </h2>{" "}
-        {profile1.map((profile, index) => (
-          <TestimonialCard key={index} profile={profile} />
-        ))}{" "}
-      </div>{" "}
-    </section>
+    <View style={styles.container}>
+      <Text style={styles.title}> Profile </Text>{" "}
+      <View style={styles.profileContainer}>
+        <View style={styles.profileImageContainer}>
+          <Image source={{ uri: "https://via.placeholder.com/150" }} style={styles.profileImage} />{" "}
+        </View>{" "}
+        <View style={styles.profileInfoContainer}>
+          <TextInput placeholder="First Name" value={firstName} onChangeText={(text) => setFirstName(text)} style={styles.textInput} /> <TextInput placeholder="Last Name" value={lastName} onChangeText={(text) => setLastName(text)} style={styles.textInput} /> <TextInput placeholder="Email" value="example@example.com" editable={false} style={[styles.textInput, styles.disabledTextInput]} /> <TextInput placeholder="Birthdate" value={birthdate} onChangeText={(text) => setBirthdate(text)} style={styles.textInput} /> <TextInput placeholder="Phone Number" value={phoneNumber} onChangeText={(text) => setPhoneNumber(text)} style={styles.textInput} />{" "}
+        </View>{" "}
+      </View>{" "}
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  profileContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 32,
+  },
+  profileImageContainer: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    overflow: "hidden",
+    marginRight: 16,
+  },
+  profileImage: {
+    width: "100%",
+    height: "100%",
+  },
+  profileInfoContainer: {
+    flexShrink: 1,
+    flexGrow: 1,
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "#ccc",
+    borderRadius: 4,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    marginBottom: 16,
+    fontSize: 16,
+  },
+  disabledTextInput: {
+    backgroundColor: "#eee",
+    color: "#666",
+  },
+});
 
 export default Profile;
